3a566803b176744994a2ef0e569cd705b57fe99b
dhruvht612006 <dhruv.thakar@ontariotechu.net>
2025-07-25 13:37:19 -0400

    Refactor ContactManager to use Contact objects instead of String arrays for better type safety and clarity

--- ContactManagerApp_FEU_2024.java
+++ ContactManagerApp_FEU_2024.java
@@ -201,142 +201,96 @@ class Contact {
 
 // Class to manage the contact list
 class ContactManager {
-    // Maximum number of contacts that can be stored
     public static final int MAX_CONTACTS = 100;
-    // Array to store contact information
-    public String[][] contacts;
-    // Number of contacts currently stored
-    public int numContacts;
+    private Contact[] contacts;
+    private int numContacts;
 
-    // Constructor to initialize the contact manager
     public ContactManager() {
-        contacts = new String[MAX_CONTACTS][];
+        contacts = new Contact[MAX_CONTACTS];
         numContacts = 0;
     }
 
-    // Method to add a contact to the contact list
     public void addContact(Contact contact) {
         if (numContacts < MAX_CONTACTS) {
-            contacts[numContacts] = new String[4 + contact.getAddresses().length];
-            contacts[numContacts][0] = contact.getName();
-            contacts[numContacts][1] = contact.getPhoneNumber();
-            contacts[numContacts][2] = contact.getEmailAddress();
-            contacts[numContacts][3] = Integer.toString(contact.getAddresses().length);
-            System.arraycopy(contact.getAddresses(), 0, contacts[numContacts], 4, contact.getAddresses().length);
+            contacts[numContacts] = contact;
             numContacts++;
-            bubbleSortByName(numContacts);  // Use recursive bubble sort to sort the contacts array by name
+            bubbleSortByName(numContacts);
             System.out.println("Contact added successfully.");
         } else {
             System.out.println("Cannot add more contacts. Contact list is full.");
         }
     }
 
-    // Recursive method to sort contacts by name using bubble sort
-    public void bubbleSortByName(int n) {
-        // Base case: if there is only one element left, stop the recursion
+    // Recursive bubble sort based on contact name
+    private void bubbleSortByName(int n) {
         if (n == 1) return;
 
-        // Perform one pass of bubble sort
         for (int i = 0; i < n - 1; i++) {
-            if (contacts[i][0].compareToIgnoreCase(contacts[i + 1][0]) > 0) {
-                // Swap contacts[i] and contacts[i + 1]
-                String[] temp = contacts[i];
+            if (contacts[i].getName().compareToIgnoreCase(contacts[i + 1].getName()) > 0) {
+                Contact temp = contacts[i];
                 contacts[i] = contacts[i + 1];
                 contacts[i + 1] = temp;
             }
         }
-
-        // Recursively call bubbleSortByName for the remaining elements
         bubbleSortByName(n - 1);
     }
 
-    // Method to search for a contact by name using binary search
+    public Contact searchByName(String name) {
+        // Could use binary search since it’s sorted
+        return binarySearchByName(name);
+    }
+
     public Contact binarySearchByName(String name) {
         int left = 0;
         int right = numContacts - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
-            int comparison = contacts[mid][0].compareToIgnoreCase(name);
-
-            if (comparison == 0) {
-                // If the contact is found, return the contact object
-                String[] addresses = Arrays.copyOfRange(contacts[mid], 4, 4 + Integer.parseInt(contacts[mid][3]));
-                return new Contact(contacts[mid][0], contacts[mid][1], contacts[mid][2], addresses);
-            } else if (comparison < 0) {
-                // If the name is greater, ignore the left half
-                left = mid + 1;
-            } else {
-                // If the name is smaller, ignore the right half
-                right = mid - 1;
-            }
+            int cmp = contacts[mid].getName().compareToIgnoreCase(name);
+            if (cmp == 0) return contacts[mid];
+            else if (cmp < 0) left = mid + 1;
+            else right = mid - 1;
         }
-        // Return null if the contact is not found
+
         return null;
     }
 
-    // Method to search for a contact by phone number
     public Contact searchByPhoneNumber(String phoneNumber) {
         for (int i = 0; i < numContacts; i++) {
-            if (contacts[i][1].equals(phoneNumber)) {
-                // If the contact is found, return the contact object
-                String[] addresses = Arrays.copyOfRange(contacts[i], 4, 4 + Integer.parseInt(contacts[i][3]));
-                return new Contact(contacts[i][0], contacts[i][1], contacts[i][2], addresses);
+            if (contacts[i].getPhoneNumber().equals(phoneNumber)) {
+                return contacts[i];
             }
         }
-        // Return null if the contact is not found
         return null;
     }
 
-    // Method to search for a contact by name using linear search
-    public Contact searchByName(String name) {
-        for (int i = 0; i < numContacts; i++) {
-            if (contacts[i][0].equalsIgnoreCase(name)) {
-                // If the contact is found, return the contact object
-                String[] addresses = Arrays.copyOfRange(contacts[i], 4, 4 + Integer.parseInt(contacts[i][3]));
-                return new Contact(contacts[i][0], contacts[i][1], contacts[i][2], addresses);
-            }
-        }
-        // Return null if the contact is not found
-        return null;
-    }
-
-    // Method to update contact information
     public void updateContact(Contact contact, String newPhoneNumber, String newEmailAddress, String[] newAddresses) {
         for (int i = 0; i < numContacts; i++) {
-            if (contacts[i][0].equalsIgnoreCase(contact.getName())) {
-                // Update the contact information
-                contacts[i][1] = newPhoneNumber;
-                contacts[i][2] = newEmailAddress;
-                contacts[i][3] = Integer.toString(newAddresses.length);
-                contacts[i] = Arrays.copyOf(contacts[i], 4 + newAddresses.length);
-                System.arraycopy(newAddresses, 0, contacts[i], 4, newAddresses.length);
-                bubbleSortByName(numContacts);  // Use recursive bubble sort to sort the contacts array by name
+            if (contacts[i].getName().equalsIgnoreCase(contact.getName())) {
+                contacts[i].setPhoneNumber(newPhoneNumber);
+                contacts[i].setEmailAddress(newEmailAddress);
+                contacts[i].setAddresses(newAddresses);
+                bubbleSortByName(numContacts);
                 System.out.println("Contact updated successfully.");
                 return;
             }
         }
-        // Print a message if the contact is not found
         System.out.println("Contact not found.");
     }
 
-    // Method to display all contacts
     public void displayContacts() {
         if (numContacts == 0) {
-            // Print a message if the contact list is empty
             System.out.println("Contact list is empty.");
             return;
         }
-        // Display each contact's information
+
         System.out.println("Contact List:");
         for (int i = 0; i < numContacts; i++) {
-            System.out.println("Name: " + contacts[i][0]);
-            System.out.println("Phone Number: " + contacts[i][1]);
-            System.out.println("Email Address: " + contacts[i][2]);
-            System.out.println("Addresses:");
-            for (int j = 4; j < 4 + Integer.parseInt(contacts[i][3]); j++) {
-                System.out.println(contacts[i][j]);
-            }
+            Contact c = contacts[i];
+            System.out.println("Name: " + c.getName());
+            System.out.println("Phone Number: " + c.getPhoneNumber());
+            System.out.println("Email Address: " + c.getEmailAddress());
+            System.out.println("Addresses: " + Arrays.toString(c.getAddresses()));
             System.out.println("--------------------");
         }
     }
